8906d41fc992920404c4c9e36f57d9fe94fb3ceb
David Ballesteros <davibq@gmail.com>
2014-12-31 13:04:18 -0600

    Styles

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ links-listing
 =============
 
 Web app to list interesting URLs. It uses Parse for backend.
+
+The app was created using this Yeoman generator https://github.com/yeoman/generator-angular
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('linksListingApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
   });
